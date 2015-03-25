129d60f6e2ab6dc04fdf25cf1782e9a3b0b12dd3
Jean-Serge <j.monbailly@orange.Fr>
2015-03-25 10:55:57 +0100

    Implémentation des méthodes de l'interface

--- src/entite/SiteImpl.java
+++ src/entite/SiteImpl.java
@@ -2,8 +2,16 @@ package entite;
 
 public class SiteImpl implements SiteItf {
 
+	/*
+	 * ==========================================================================
+	 * Constructeurs et attributs 
+	 * ================================
+	 */
+	
+	
 	private SiteItf pere;
 	private SiteItf fils[];
+	private int id;
 	
 	/**
 	 * Instancie un nouveau site avec un pere et une liste de fils.
@@ -11,22 +19,53 @@ public class SiteImpl implements SiteItf {
 	 * @param pere le père du Site courant
 	 * @param fils les files du Site courant
 	 */
-	public SiteImpl(SiteItf pere, SiteItf[] fils) {
+	public SiteImpl(int id, SiteItf pere, SiteItf[] fils) {
 		super();
 		this.pere = pere;
 		this.fils = fils;
+		this.id = id;
 	}
 
+	/*
+	 * ==========================================================================
+	 * Fonctions implémentées 
+	 * ================================
+	 */
+	
+	/**
+	 * Transfère les données passées en paramètre à chacun des fils 
+	 * du site courant.
+	 * 
+	 * @param donnees le message à transmettre aux fils
+	 */
 	@Override
 	public void transfererAuxFils(byte[] donnees) {
-		// TODO Auto-generated method stub
-		
+		for(SiteItf s : fils){
+			System.out.println("Transfert de données de " + id + " à " + s.getId());
+			s.recevoir(donnees);
+		}
 	}
 
+	
+	/**
+	 * Permet à un Site de recevoir des données de son père.
+	 * 
+	 * @param donnees les données reçues du père
+	 */
 	@Override
 	public void recevoir(byte[] donnees) {
-		// TODO Auto-generated method stub
-		
+		this.transfererAuxFils(donnees);
 	}
 
+	/*
+	 * ==========================================================================
+	 * Accesseurs 
+	 * ================================
+	 */
+	
+	public int getId() {
+		return id;
+	}
+
+	
 }
