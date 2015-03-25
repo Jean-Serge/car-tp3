25669b1a5b9c59abc16d1a0b42f11f4758ec1427
Jean-Serge <j.monbailly@orange.Fr>
2015-03-25 10:46:12 +0100

    Ajout du squelette vide des fonction à implémenter

---
+++ src/entite/SiteImpl.java
@@ -0,0 +1,17 @@
+package entite;
+
+public class SiteImpl implements SiteItf {
+
+	@Override
+	public void transfererAuxFils(byte[] donnees) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void recevoir(byte[] donnees) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
