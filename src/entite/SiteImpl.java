0644069232aec098f4465210d3df15c809592303
Jean-Serge <j.monbailly@orange.Fr>
2015-03-25 10:48:00 +0100

    Ajout des attributs de la classe

--- src/entite/SiteImpl.java
+++ src/entite/SiteImpl.java
@@ -2,6 +2,21 @@ package entite;
 
 public class SiteImpl implements SiteItf {
 
+	private SiteItf pere;
+	private SiteItf fils[];
+	
+	/**
+	 * Instancie un nouveau site avec un pere et une liste de fils.
+	 * 
+	 * @param pere le père du Site courant
+	 * @param fils les files du Site courant
+	 */
+	public SiteImpl(SiteItf pere, SiteItf[] fils) {
+		super();
+		this.pere = pere;
+		this.fils = fils;
+	}
+
 	@Override
 	public void transfererAuxFils(byte[] donnees) {
 		// TODO Auto-generated method stub
