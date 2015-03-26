0165ebff9f177e113f34f475f5a0844af73adddd
Jean-Serge <j.monbailly@orange.Fr>
2015-03-26 11:11:52 +0100

    Documentation et mise en page

--- src/rmi/ClientRMI.java
+++ src/rmi/ClientRMI.java
@@ -5,7 +5,6 @@ import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
-import entite.SiteImpl;
 import entite.SiteItf;
 
 import utils.Tools;
@@ -13,13 +12,22 @@ import utils.Tools;
 public class ClientRMI {
 
 	/**
+	 * Cette classe sert de client au Serveur RMI lancé au préalable.
+	 * 
 	 * @param args
-	 * @throws RemoteException 
-	 * @throws NotBoundException 
+	 * @throws RemoteException
+	 * @throws NotBoundException
 	 */
-	public static void main(String[] args) throws RemoteException, NotBoundException {
-		Registry r;
+	public static void main(String[] args) throws RemoteException,
+			NotBoundException {
+		/*
+		 * Ce client se connecte au Serveur RMI pour récupérer s1 (la racine de
+		 * l'arbre). Il lui envoie ensuite un message et on peut constater le
+		 * résultat.
+		 */
 		
+		Registry r;
+
 		r = LocateRegistry.getRegistry(Tools.PORT_RMI_SERVEUR);
 		SiteItf s1 = (SiteItf) r.lookup("s1");
 		s1.transfererAuxFils("Bonjour".getBytes());
