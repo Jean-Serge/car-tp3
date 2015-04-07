1004909dd5a1e43ed71550fdf589d426da17814b
Jean-Serge <j.monbailly@orange.Fr>
2015-04-07 09:33:22 +0200

    Ajout de la fonction testant ajouterVoisin

--- test/entite/TestSite.java
+++ test/entite/TestSite.java
@@ -11,7 +11,7 @@ import org.junit.Test;
 
 public class TestSite {
 
-	private SiteImpl s1, s2, s3, s4, s5, s6;
+	private SiteItf s1, s2, s3, s4, s5, s6;
 
 	@Before
 	public void setUp() throws RemoteException {
@@ -23,20 +23,40 @@ public class TestSite {
 		s4 = new SiteImpl();
 	}
 	
+	/**
+	 * Cette fonction teste le bon fonctionnement de la fonction init de la
+	 * classe SiteImpl.
+	 * 
+	 * @throws RemoteException
+	 */
 	@Test
 	public void testInit() throws RemoteException{
-		s1.init(1, null);
-		s2.init(2, s1);
-		s5.init(5, s1);
-		s6.init(6, s5);
-		s3.init(3, s2);
-		s4.init(4, s2);
+		s1.init(1);
+		s6.init(6);
 		
+		// Vérification des ids
 		assertEquals(1, s1.getId());
 		assertEquals(6, s6.getId());
 		
-		assertFalse(s1.getVoisins().isEmpty());
-		assertTrue(s3.getVoisins().contains(s2));
+		// Initialise une liste vide
+		assertTrue(s1.getVoisins().isEmpty());
+	}
+	
+	
+	/**
+	 * Cette fonction vérifie le bon fonctionnement de la fonction d'ajout 
+	 * d'un voisin à un site.
+	 * 
+	 * @throws RemoteException
+	 */
+	@Test
+	public void testAjoutVoisin() throws RemoteException{
+		s1.init(1);
+		s2.init(2);
+		
+		s1.ajouterSite(s2);
+		
 		assertTrue(s1.getVoisins().contains(s2));
+		assertTrue(s2.getVoisins().contains(s1));
 	}
 }
